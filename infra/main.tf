9b61b5259a29d186c9e44bc784bf0ab200647db9
Aditya Tapshalkar <aditya.tapshalkar@finastra.com>
2026-07-14 10:28:50 -0700

    infra: terraform bootstrap — providers, variables, outputs

---
+++ infra/main.tf
@@ -0,0 +1,34 @@
+terraform {
+  required_version = ">= 1.6"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.region
+}
+
+provider "cloudflare" {
+  api_token = var.cloudflare_api_token
+}
+
+data "aws_caller_identity" "current" {}
+
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnets" "public" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
---
+++ infra/variables.tf
@@ -0,0 +1,19 @@
+variable "region" {
+  default = "us-west-2"
+}
+
+variable "app_name" {
+  default = "payinvestigator"
+}
+
+variable "domain" {
+  default = "vistahack26.tapshalkar.com"
+}
+
+variable "cloudflare_zone_id" {
+  default = "9a2b68936aec95fc2ad33a144cec981a"
+}
+
+variable "cloudflare_api_token" {
+  sensitive = true
+}
